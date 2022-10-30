8410c8f2d81152091e20fc14cb1fc37ccd7c8732
Vovere/Rokas <48134959+voovere@users.noreply.github.com>
2022-10-30 16:26:26 +0200

    Init

---
+++ app/src/main/java/com/example/a3lab/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a3lab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
